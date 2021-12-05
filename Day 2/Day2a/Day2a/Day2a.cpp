54422b0239887477a9b8846a20c57a8a99c29bb2
Pickle <44123014+esteemedtogami@users.noreply.github.com>
2021-12-05 10:43:03 -0500

    Day 2 Upload

---
+++ Day 2/Day2a/Day2a/Day2a.cpp
@@ -0,0 +1,50 @@
+/*
+    Programmer: Sam Pickell
+    Date: 12/3/21
+    Filename: Day2a.cpp
+    Description: Find out where the submarine is
+                 going based on multiplying position
+                 and depth.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    // Variables
+    int hor_pos = 0, depth = 0;
+    std::string my_dir;
+    std::string my_val;
+    std::ifstream my_file("SubPath.txt");
+
+    if (my_file.is_open())
+    {
+        // Iterate over each word in the file
+        while (my_file >> my_dir)
+        {
+            my_file >> my_val;
+
+            // Update approriate var based on direction
+            if (my_dir == "forward")
+            {
+                hor_pos += std::stoi(my_val);     
+            }
+            else if (my_dir == "up")
+            {
+                depth -= std::stoi(my_val);
+            }
+            else if (my_dir == "down")
+            {
+                depth += std::stoi(my_val);
+            }
+        }
+    }
+
+    my_file.close();
+
+    std::cout << "Final position: " << hor_pos * depth << std::endl;
+
+    return 0;
+}
